b32f079ba0f83d4c7725255672ee34dd2e8422c2
Igor From <ig.from@yandex.ru>
2023-04-02 21:56:51 +0700

    add hw5

---
+++ sem5.java
@@ -0,0 +1,58 @@
+
+/**
+ * Есть тест. Нужно извлечь из него все слова и отсортировать по длине.
+ * Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись
+ *
+ * и
+ * ты
+ * Еще
+ * день друг Пора
+ * ..
+ *
+ *
+*static void collectStats(String text) {
+    // Map<Integer, List<String>> stats = new HashMap<>();
+}
+    */
+
+
+package Homeworks;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class sem5 {
+    public static void main(String[] args) {
+        
+        String text = "Мороз и солнце день чудесный, Еще ты дремлешь друг прелестный, Пора красавица проснись";
+        collectStats(text);
+        }
+    static void collectStats(String text) {
+        text = removePunctuations(text);
+        String [] words = text.split((" "));
+        LinkedList<String> res = new LinkedList<>();
+        System.out.println(Arrays.toString(words));
+        Map<Integer, List<String>> result = new HashMap<>();
+            for (int i = 0; i < words.length; i++) {
+                for (String s : words) {
+                    if (s.length() == i) {
+                        result.put(i, Collections.singletonList(s));
+                        if (result.get(i) != null) {
+                            res.add(s);
+                        }
+                        result.put(i, res);
+                    }
+                }
+                res = new LinkedList<>();
+            }
+        System.out.println(result);
+        }
+
+    public static String removePunctuations(String source) {
+        return source.replaceAll("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]", "");
+    }
+  }
